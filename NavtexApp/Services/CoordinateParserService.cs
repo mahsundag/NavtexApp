a060c001af4c17c2d2a28134837c650f4b3b1f5a
mahsun.dag <mahsun.dag@atolla.com.tr>
2023-09-16 14:51:20 +0300

    Get Exact Text is Done

--- NavtexApp/Services/CoordinateParserService.cs
+++ NavtexApp/Services/CoordinateParserService.cs
@@ -4,14 +4,23 @@
     {
         public string GetExactText(string text)
         {
-            return text;
+            int startIndex = text.IndexOf("ZCZC");
+            int endIndex = text.IndexOf("NNNN",startIndex);
+
+            if (startIndex == -1 || endIndex == -1 || endIndex < startIndex)
+                return null;
+
+            if (endIndex + 4 >= text.Length)  
+                return null;
+
+            return text.Substring(startIndex, (endIndex + 4) - startIndex);
         }
 
         public string Highlight(string text)
         {
            var result = GetExactText(text);
            var coordinates = Parse(result);
-           return "<span class=\"highlighted\">vurgulanan</span>";
+           return $"<span class=\"highlighted\">{result}</span>";
         }
 
         public string Parse(string text)
