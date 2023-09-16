fe4b01b5aa18cbe6e2f87904867094ecd4fa3d54
mahsun.dag <mahsun.dag@atolla.com.tr>
2023-09-16 14:55:33 +0300

    Test Project Added and First test applied

---
+++ NavtexApp.UnitTest/CoordinateParserServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using NavtexApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavtexApp.UnitTest
+{
+    public class CoordinateParserServiceTests
+    {
+        private readonly ICoordinateParserService _service;
+
+        public CoordinateParserServiceTests()
+        {
+            _service = new CoordinateParserService();
+        }
+
+        [Fact]
+        public void GetExactText_ExtractsCorrectly()
+        {
+            // Arrange
+            var inputText = "randomTextZCZC EA46\nWZ 700\nWALES ... BELL INOPERATIVE.\nNNNNasdlasdlsakd";
+            var expectedText = "ZCZC EA46\nWZ 700\nWALES ... BELL INOPERATIVE.\nNNNN";
+
+            // Act
+            var result = _service.GetExactText(inputText);
+
+            // Assert
+            Assert.Equal(expectedText, result);
+        }
+    }
+}
