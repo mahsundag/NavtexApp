af0916e8f8af31b00abc92b075e76fe90814eb05
mahsun.dag <mahsun.dag@atolla.com.tr>
2023-09-16 13:54:19 +0300

    Add project files.

---
+++ NavtexApp/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NavtexApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public IFormFile UploadedFile { get; set; }
+
+        public string NavtexMessage { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public async Task OnPostAsync()
+        {
+            if (UploadedFile != null && UploadedFile.Length > 0)
+            {
+                using var reader = new StreamReader(UploadedFile.OpenReadStream());
+                NavtexMessage = await reader.ReadToEndAsync();
+
+                
+            }
+        }
+
+    }
+}
---
+++ NavtexApp/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function uploadFile() {
+    var formData = new FormData(document.getElementById('uploadForm'));
+
+    fetch('/Home/Upload', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navtexMessage').innerHTML = data;
+        });
+}
